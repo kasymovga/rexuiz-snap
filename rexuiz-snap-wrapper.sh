8cea39b20ee7732b61d06c8943592ec9b108d2f6
Grigorii Kasymov <kasymovga@gmail.com>
2021-02-07 10:27:21 +0500

    wrapper improvements

--- rexuiz-snap-wrapper.sh
+++ rexuiz-snap-wrapper.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-export LIBGL_DRIVERS_PATH="$SNAP/usr/lib/x86_64-linux-gnu/dri"
+if test -n "$LIBGL_DRIVERS_PATH"
+then
+	export LIBGL_DRIVERS_PATH="$LIBGL_DRIVERS_PATH:$SNAP/usr/lib/x86_64-linux-gnu/dri"
+else
+	export LIBGL_DRIVERS_PATH="$SNAP/usr/lib/x86_64-linux-gnu/dri"
+fi
 if test -n "$LD_LIBRARY_PATH"
 then
 	export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$SNAP/usr/lib/x86_64-linux-gnu/pulseaudio"
